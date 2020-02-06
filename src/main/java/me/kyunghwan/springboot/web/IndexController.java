fc823ec01f2cdac9df410ad229e5c913bf1fc702
ber01 <ber01@naver.com>
2020-02-07 03:02:03 +0900

    Added 게시글 수정 작성

--- src/main/java/me/kyunghwan/springboot/web/IndexController.java
+++ src/main/java/me/kyunghwan/springboot/web/IndexController.java
@@ -2,9 +2,11 @@ package me.kyunghwan.springboot.web;
 
 import lombok.RequiredArgsConstructor;
 import me.kyunghwan.springboot.service.posts.PostsService;
+import me.kyunghwan.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,6 +23,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 
 }
