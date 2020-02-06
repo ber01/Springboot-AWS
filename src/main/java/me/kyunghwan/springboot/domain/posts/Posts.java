1118f6a0d4c5af0132c23b3e064ae7e6726346ef
ber01 <ber01@naver.com>
2020-02-06 17:36:53 +0900

    Added JPA Auditing

--- src/main/java/me/kyunghwan/springboot/domain/posts/Posts.java
+++ src/main/java/me/kyunghwan/springboot/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package me.kyunghwan.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import me.kyunghwan.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
