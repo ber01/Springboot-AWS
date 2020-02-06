3c1eb261f4bdfe0b96994ee650fff69e13fa13ad
ber01 <ber01@naver.com>
2020-02-06 23:32:42 +0900

    Added 게시글 등록, 전체 게시글 조회 작성

--- src/main/java/me/kyunghwan/springboot/service/posts/PostsService.java
+++ src/main/java/me/kyunghwan/springboot/service/posts/PostsService.java
@@ -3,11 +3,15 @@ package me.kyunghwan.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import me.kyunghwan.springboot.domain.posts.Posts;
 import me.kyunghwan.springboot.domain.posts.PostsRepository;
+import me.kyunghwan.springboot.web.dto.PostsListResponseDto;
 import me.kyunghwan.springboot.web.dto.PostsResponseDto;
 import me.kyunghwan.springboot.web.dto.PostsSaveRequestDto;
 import me.kyunghwan.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
