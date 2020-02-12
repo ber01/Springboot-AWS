bf5a211e1970f6daf21c9ce6d1afb208763975d7
ber01 <ber01@naver.com>
2020-02-12 22:54:03 +0900

    Added LoginUser 설정 추가
    - 현재 사용자 어노테이션 기반으로 수정

--- src/main/java/me/kyunghwan/springboot/web/IndexController.java
+++ src/main/java/me/kyunghwan/springboot/web/IndexController.java
@@ -1,6 +1,7 @@
 package me.kyunghwan.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import me.kyunghwan.springboot.config.auth.LoginUser;
 import me.kyunghwan.springboot.config.auth.dto.SessionUser;
 import me.kyunghwan.springboot.service.posts.PostsService;
 import me.kyunghwan.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null ) {
             model.addAttribute("userName", user.getName());
         }
