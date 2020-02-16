3d279744fc049a28098166a9caba520cfe9e8d4d
ber01 <ber01@naver.com>
2020-02-16 23:34:34 +0900

    Updated PostsRepositoryTest, HelloControllerTest
    - 테스트 통과하게 수정

--- src/test/java/me/kyunghwan/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/me/kyunghwan/springboot/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class PostsRepositoryTest {
 
     @Autowired
